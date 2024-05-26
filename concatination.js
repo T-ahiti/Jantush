80cce28a983f8f783a173d88d86fa815af972b1d
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-26 22:11:33 +0600

    file added

---
+++ concatination.js
@@ -0,0 +1,16 @@
+var Price1 = 30;
+var Price2 = 40;
+var totalPrice = Price1 + Price2;
+// console.log(totalPrice);
+
+// number plus kora jay but words/string egula jora lage just
+var firstName = 'Jennie' ;
+var lastName = 'kim';
+var fullName = firstName + lastName;
+console.log(fullName);
+
+// namer majhe gap dewar jonno 2ta coation er modhhe break dite hobe
+var firstName = 'Jennie' ;
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
