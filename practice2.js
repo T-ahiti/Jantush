cacfabc84832bf111b0f214c63689d514a21533a
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-27 02:00:48 +0600

    prac 2 added

---
+++ practice2.js
@@ -0,0 +1,21 @@
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bangla = 99.50;
+
+var total = (math + bangla + bio + chem +phy);
+
+average = total / 5;
+average = average.toFixed(2);
+console.log(average);
+
+
+
+
+
+
+
+
+
+
