87dcd7188c96bf173d4bd77dfc3b19c4fd6aeb60
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-26 22:11:46 +0600

    file added

--- conversion.js
+++
@@ -1,9 +0,0 @@
-var Price1 = 30;
-var Price2 = 40;
-var totalPrice = Price1 + Price2;
-// console.log(totalPrice);
-
-var firstName = 'Jennie' ;
-var lastName = 'kim';
-var fullName = firstName + lastName;
-console.log(fullName);
