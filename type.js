5ae49927802f3badb60ea25127b4eb5b0faabd58
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-27 01:41:06 +0600

    fix result

---
+++ type.js
@@ -0,0 +1,19 @@
+// type of likhle price ta ki type er value ta bolbe
+var price =100;
+console.log(typeof price);
+
+var age = '100';
+console.log(typeof age);
+
+var isHappy = true ;
+console.log(typeof isHappy);
+
+var romantic;
+console.log(typeof romantic);
+
+// .1 r .2 plus korle 3.000000004 dekhay. eta solve korar jonno point er por koy ghor porjonto answer hobe seta fixed kora jay
+var num1 = .1;
+var num2 = .2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+console.log(sum);
